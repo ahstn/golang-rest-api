57fa1f5f9a675b0440074d2e5d418b61ac2b7928
Adam Houston <phazyy9@gmail.com>
2017-02-11 16:23:44 +0000

    Adding create pilot endpoint

--- daos/pilots.go
+++ daos/pilots.go
@@ -3,17 +3,30 @@ package daos
 import (
 	"database/sql"
 
-	"github.com/phazyy/sqlboiler-gin/models"
+	"github.com/phazyy/golang-rest-api/models"
 	"gopkg.in/inconshreveable/log15.v2"
 )
 
-func GetPilots(db *sql.DB, log log15.Logger) models.PilotSlice {
+func GetAll(db *sql.DB, log log15.Logger) models.PilotSlice {
 	pilots, err := models.Pilots(db).All()
 	if err != nil {
-		log.Error("failed to get pilots", "err", err)
+		log.Error("db: failed to get pilots", "err", err)
 		return nil
 	}
 
-	log.Info("db: fetched pilots")
+	log.Info("db: fetched pilots", "count", len(pilots))
 	return pilots
+}
+
+func Create(name string, db *sql.DB, log log15.Logger) int {
+	pilot := &models.Pilot{
+		Name: name,
+	}
+	if err := pilot.Insert(db); err != nil {
+		log.Error("db: failed to insert pilot", "err", err)
+		return 0
+	}
+
+	log.Info("db: insterted pilot", "id", pilot.ID)
+	return pilot.ID
 }
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
 	"github.com/phazyy/golang-rest-api/daos"
+	"github.com/phazyy/golang-rest-api/models"
 	"gopkg.in/inconshreveable/log15.v2"
 )
 
@@ -15,10 +16,19 @@ func main() {
 	r := gin.Default()
 
 	db := daos.Open(log)
-	pilots := daos.GetPilots(db, log)
 
 	r.GET("/pilots", func(c *gin.Context) {
+		pilots := daos.GetAll(db, log)
 		c.JSON(200, pilots)
+	})
+
+	r.POST("/pilots", func(c *gin.Context) {
+		var json models.Pilot
+		if c.BindJSON(&json) != nil {
+			log.Error("gin: error creating user")
+		}
+
+		daos.Create(json.Name, db, log)
 	})
 
 	r.Run(":8080")
